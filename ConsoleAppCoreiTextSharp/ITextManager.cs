369514e0663db21ad06e0739232f96dbea4d9684
Mohamed ELBAKKALI <med.el.developer@gmail.com>
2020-08-03 16:00:39 +0100

    Add project files.

---
+++ ConsoleAppCoreiTextSharp/ITextManager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using iText.Layout.Properties;
+
+namespace ConsoleAppCoreiTextSharp
+{
+    class ITextManager
+    {
+        public void Write(string path, string text)
+        {
+            // Must have write permissions to the path folder 
+           /* string pat = "‪oncf_voyages.pdf";
+            PdfWriter writer = new PdfWriter(pat);
+            PdfDocument pdf = new PdfDocument(writer);
+            Document document = new Document(pdf);
+            Paragraph header = new Paragraph(text)
+               .SetTextAlignment(TextAlignment.CENTER)
+               .SetFontSize(20);
+
+            document.Add(header);
+
+            document.Close();*/
+
+
+
+
+            string pat = @"‪‪C:\Users\Hp ProBook\source\repos\ConsoleAppCoreiTextSharp\ConsoleAppCoreiTextSharp\bin\Debug\netcoreapp3.1\ADD1-TD2.pdf";
+
+
+            //PdfWriter writer = new PdfWriter(pat);
+
+
+            if (File.Exists(pat))
+            {
+
+                var pdfReader = new PdfReader(pat);
+
+                PdfDocument pdf = new PdfDocument(pdfReader, new PdfWriter("‪oncf_voyagesv2.pdf"));
+
+
+                Document document = new Document(pdf);
+                Paragraph header = new Paragraph(text)
+                   .SetTextAlignment(TextAlignment.CENTER)
+                   .SetFontSize(20);
+
+                document.Add(header);
+
+                document.Close();
+            }
+            else
+            {
+                Console.Write("PDF Not Found");
+            }
+            
+
+        }
+            
+        }
+}
---
+++ ConsoleAppCoreiTextSharp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleAppCoreiTextSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ITextManager textManager = new ITextManager();
+            textManager.Write("‪mypdf.pdf", "Center Header") ;
+
+            Console.WriteLine("Done!");
+        }
+    }
+}
